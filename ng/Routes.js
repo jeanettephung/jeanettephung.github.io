da77e5ebd39233fe685b7f17ac36a2abe5b258f4
JeanettePhung <j5phung@ucsd.edu>
2016-11-27 12:20:11 -0800

    completed three projects page (about and 4th project pending)

--- ng/Routes.js
+++ ng/Routes.js
@@ -5,4 +5,7 @@ angular.module('app')
             .when('/contact',{controller:'ContactCtrl', templateUrl:'/templates/contact.html'})
             .when('/about',{templateUrl:'/templates/about.html'})
 						.when('/pigment',{templateUrl:'/templates/pigment.html'})
+						.when('/bridge',{templateUrl:'/templates/bridge.html'})
+						.when('/win8',{templateUrl:'/templates/win8.html'})
+						.when('/cogs',{templateUrl:'/templates/cogs.html'})
     });
